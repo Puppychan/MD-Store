ee2bb60f406cf80d503ce000d26a7c3609c225ab
Matsuri <nhungmaitran1412@gmail.com>
2022-07-08 16:36:57 +0700

    len

--- js/nav-script.js
+++ js/nav-script.js
@@ -1,5 +1,11 @@
+// header
 let menuIcon = document.querySelector(".body-header-menuicon");
 let header = document.querySelector(".body-header");
+//main
+
+
+
+// event listener
 menuIcon.addEventListener("click", event => {
     header.classList.toggle("open");
 })
